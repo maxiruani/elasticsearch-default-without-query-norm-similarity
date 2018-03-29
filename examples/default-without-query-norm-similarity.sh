64aafa78dbc9704031861376a57cb38d270eebd3
Maximiliano Ruani <maximiliano@restorationmedia.com>
2018-03-29 20:06:41 -0300

    - Improved scripts - Improved example

---
+++ examples/default-without-query-norm-similarity.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+
+curl -s -XDELETE "http://localhost:9200/test_index"
+
+curl -s -XPUT "http://localhost:9200/test_index" -d '
+{
+  "settings": {
+    "index": {
+      "number_of_shards": 1,
+      "number_of_replicas": 0
+    },
+    "similarity": {
+      "defaultWithoutQueryNormSimilarity": {
+        "type": "default-without-query-norm-similarity"
+      }
+    }
+  }
+}
+'
+
+curl -XPUT 'localhost:9200/test_index/test_type/_mapping' -d '
+{
+  "test_type": {
+    "properties": {
+      "field1": {
+        "type": "string",
+        "analyzer": "standard"
+      },
+      "field2": {
+        "type": "string",
+        "analyzer": "standard",
+        "similarity": "defaultWithoutQueryNormSimilarity"
+      }
+    }
+  }
+}
+'
+
+curl -s -XPUT "localhost:9200/test_index/test_type/1" -d '
+{"field1" : "customer service representative", "field2" : "customer service representative"}
+'
+
+curl -s -XPUT "localhost:9200/test_index/test_type/2" -d '
+{"field1" : "customer service", "field2" : "customer service"}
+'
+
+curl -s -XPUT "localhost:9200/test_index/test_type/3" -d '
+{"field1" : "customer support", "field2" : "customer support"}
+'
+
+curl -s -XPOST "http://localhost:9200/test_index/_refresh"
+
+echo
+echo
+echo 'expecting to match all with integer scores'
+
+curl -s "localhost:9200/test_index/test_type/_search?pretty=true" -d '
+{
+  "explain": false,
+  "query": {
+    "match": {
+      "field2": "customer service representative",
+      "operator": "or"
+    }
+  }
+}
+'
+
+echo
+echo
+echo 'explain highest score'
+
+curl -s "localhost:9200/test_index/test_type/_search?pretty=true" -d '
+{
+  "explain": true,
+  "from": 0,
+  "size": 1,
+  "query": {
+    "match": {
+      "field2": "customer service representative",
+      "operator": "or"
+    }
+  }
+}
+'
--- scripts/install-plugin.sh
+++ scripts/install-plugin.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/local/opt/elasticsearch-2.4.3/bin/plugin install file:./target/releases/elasticsearch-position-similarity-2.4.3.zip
+/usr/local/opt/elasticsearch-2.4.1/bin/plugin install file:./target/releases/elasticsearch-default-without-query-norm-similarity-2.4.1.zip
